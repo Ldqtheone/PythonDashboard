6d6071164f76b9875450cd084cb613f14daf6ce3
Ldq <brian.lecarpentier@edu.itescia.fr>
2021-03-30 10:37:10 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""Main module
+
+Copyright (c) 2021 Brian Lecarpentier
+All Rights Reserved
+Released under the MIT license
+
+"""
+
+
+def main():
+    """ main method """
+    pass
+
+
+if __name__ == '__main__':
+    main()
