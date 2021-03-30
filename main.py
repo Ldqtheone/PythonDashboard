069d7e62d0f43195dc98804e67a34bd5d12deba2
Alyssia <alyssia0207@gmail.com>
2021-03-30 14:35:28 +0200

    Auto stash before rebase of "origin/dev"

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ Released under the MIT license
 """
 import psutil
 
+from Utils.Config.parser import *
+
+
 def main():
     """ main method """
+    get_data()
+    print(get_interval(Interval.memory.name))
     pass
 
 
 if __name__ == '__main__':
     main()
+
